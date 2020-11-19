8fd7d68a49feca8d97f5c2b054f20358aadd8869
wbytts <wbytts@163.com>
2020-11-20 00:08:57 +0800

    创建视图函数，添加URL映射

--- myProject/myProject/urls.py
+++ myProject/myProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'), # 新增路由映射
 ]
